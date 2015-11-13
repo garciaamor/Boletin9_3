4d760f3cac944e202351638b1b91dbf5c2ee735b
jgarciaamor <jgarciaamor@atanasoff01.danielcastelao.org>
2015-11-13 11:53:10 +0100

    Boletin 9

--- Boletin9_3/src/boletin9_3/Boletin9_3.java
+++ Boletin9_3/src/boletin9_3/Boletin9_3.java
@@ -3,7 +3,8 @@ package boletin9_3;
 public class Boletin9_3 {
 
     public static void main(String[] args) {
-        
+        Clase9_3 obx=new Clase9_3();
+        obx.calcularArea();
     }
     
 }
--- Boletin9_3/src/boletin9_3/Clase9_3.java
+++ Boletin9_3/src/boletin9_3/Clase9_3.java
@@ -1,5 +1,26 @@
 package boletin9_3;
 
+import javax.swing.JOptionPane;
+
 public class Clase9_3 {
     
+    public float poneraltura(){
+        return Float.parseFloat(JOptionPane.showInputDialog("Introduzca la altura "));
+    }
+    public float ponerbase(){
+        return Float.parseFloat(JOptionPane.showInputDialog("Introduzca la base "));
+    }
+    public void calcularArea(){
+        float base;
+        float altura;
+        do {
+            base=ponerbase();
+            altura=poneraltura();
+        }
+            while (base<=0||altura<=0);
+        
+        JOptionPane.showMessageDialog(null, "El area del rectangulo es : " + (base*altura));
+        
+        
+    }
 }
